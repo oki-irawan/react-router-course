bd88f655eec1ba58b3597902099cd3bfbf234298
Oki Irawan <oki.irwn@gmail.com>
2022-11-19 00:57:48 +0700

    creating players page with query params

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,7 +1,72 @@
 import React from 'react'
+import { Link, useLocation, useSearchParams } from 'react-router-dom'
+
+import usePlayerNames from '../hooks/usePlayerNames';
+import { slugify } from '../utils';
+
+function CustomLink({ to, children}) {
+  const location = useLocation();
+  const playerId = location.pathname.split('/')[2];
+  const match = playerId === to;
+
+  const style = match === true ? {fontWeight: 900, color: 'var(--white)'} : {}
+
+  return (
+    <li>
+      <Link 
+        to={{
+          pathname: to,
+          search: location.search
+        }} 
+        style={{...style}}
+      >
+        {children}
+      </Link>
+    </li>
+  )
+}
+
+
+function SideBar ({ title, list }) {
+  return (
+    <div>
+      <h3 className='header'>
+        {title}
+      </h3>
+      <ul className='sidebar-list'>
+        {list.map((item) => (
+          <CustomLink 
+            key={item} 
+            to={slugify(item)}
+          >
+            {item.toUpperCase()}
+          </CustomLink>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default function Players() {
+  const location = useLocation();
+  const [searchParams] = useSearchParams(location);
+
+  const team = searchParams.get('teamId')
+
+  const {
+    response: names,
+    loading,
+  } = usePlayerNames(team)
+
+  if (loading === true) return null;
+
+
   return (
-    <div className='container'>Players</div>
+    <div className='container'>
+      <SideBar
+        title="Players"
+        list={names}
+      />
+    </div>
   )
 }
--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -66,6 +66,7 @@ export default function TeamPage() {
             </Link>
         </h4>
 
+        <h4>Championships</h4>
         <ul className='championships'>
             {team.championships.map((ship) => <li key={ship}>{ship}</li>)}
         </ul>
