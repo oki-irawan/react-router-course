f3859f454a4fdae4755377ac8c04918bbe77912e
Oki Irawan <oki.irwn@gmail.com>
2022-11-18 23:02:17 +0700

    initial router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+import Navbar from './Navbar';
+import Players from './Players';
+import Home from './Home';
+import Teams from './Teams';
+
 export default function App () {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/players" element={<Players />}/>
+          <Route path="/teams" element={<Teams />}/>
+        </Routes>
+      </div>
+    </Router>
   )
 }
